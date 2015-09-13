42d529d796273d347bd435d93aabd9b01800373e
csturco0331 <csturco0331@yahoo.com>
2015-09-13 16:00:52 -0400

    Add priority ability and edit/complete buttons

--- todo-src/script.js
+++ todo-src/script.js
@@ -3,24 +3,71 @@
 var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
-  $scope.todos = ["Learn Angular", "Learn node"];
+  $scope.high = [];
+  $scope.medium = [];
+  $scope.low = [];
   $scope.newItem = "";
   
-  $scope.addItem = function(){
-    console.log("in add");
+  $scope.highaddItem = function(){
+    console.log("high add");
     if ($scope.newItem !== ""){
-      $scope.todos.push($scope.newItem);
+      $scope.high.push($scope.newItem);
       $scope.newItem = "";
     }
   }
-    
-  $scope.deleteItem = function(item){
-    console.log("in delete");
-    var index = $scope.todos.indexOf(item);
-    $scope.todos.splice(index, 1);
+
+  $scope.highcheckItem = function(item){
+    console.log("high check");
+    var index = $scope.high.indexOf(item);
+    //need implementation
   }
-    
-  
+
+  $scope.highdeleteItem = function(item){
+    console.log("high delete");
+    var index = $scope.high.indexOf(item);
+    $scope.high.splice(index, 1);
+  }
+
+  $scope.medaddItem = function(){
+    console.log("med add");
+    if ($scope.newItem !== ""){
+      $scope.medium.push($scope.newItem);
+      $scope.newItem = "";
+    }
+  }
+
+  $scope.medcheckItem = function(item){
+    console.log("medium check");
+    var index = $scope.medium.indexOf(item);
+    //need implementation
+  }
+
+  $scope.meddeleteItem = function(item){
+    console.log("med delete");
+    var index = $scope.medium.indexOf(item);
+    $scope.medium.splice(index, 1);
+  }
+
+  $scope.lowaddItem = function(){
+    console.log("low add");
+    if ($scope.newItem !== ""){
+      $scope.low.push($scope.newItem);
+      $scope.newItem = "";
+    }
+  }
+
+  $scope.highcheckItem = function(item){
+    console.log("low check");
+    var index = $scope.low.indexOf(item);
+    //Need implementation
+  }
+
+  $scope.lowdeleteItem = function(item){
+    console.log("low delete");
+    var index = $scope.low.indexOf(item);
+    $scope.low.splice(index, 1);
+  }
+
 });
 
 /*************************
